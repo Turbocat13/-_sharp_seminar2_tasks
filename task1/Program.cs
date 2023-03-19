9f4ca327bfa2c3c11e0fdfbace5fccb96e0cc859
Cat <ligeia_undead@mail.ru>
2023-03-19 20:18:06 +0300

    // Напишите программу, которая выводит случайное число из отрезка 10,99 и показывает наибольшую йифру числа

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+Console.Clear();
+int n = new Random().Next(10, 100); 
+Console.WriteLine($"Случайно сгенированное число {n}");
+int n1 = n / 10;
+int n2 = n % 10;
+
+if (n1 > n2) Console.WriteLine(n1);
+
+else
+Console.WriteLine(n2);
