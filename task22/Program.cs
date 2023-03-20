dab6d6f6f08718165768a03299cd0ee3779338cb
Cat <ligeia_undead@mail.ru>
2023-03-20 16:11:00 +0300

    Напишите программу, которая принимает на вход число (N) // и выдает таблицу квадратов чисел от 1 до N

---
+++ task22/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу квадратов чисел от 1 до N
+Console.Clear();
+Console.Write("Введите число для проверки: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i<=n; i++)
+    Console.Write($"{i * i} ");
+
